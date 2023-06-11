2f89fdae7cee4ce886b1497cabf42ba554ddc697
Hamdi Abdikarin <hamdiabdikarim14@gmail.com>
2023-06-11 14:45:34 +0300

    first commit

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -97,25 +97,6 @@ if(language === "English" && population < 50 &&  isIsland === true) {
 
 
 
- // coding challenge 1 //
-// Data 1//
-const massMark = 78;
-const massJohn = 92;
-const heightMark = 1.69;
-const heightJohn = 1.95;
-
-const BMIMark = massMark / (heightMark * heightMark)
-
-
-const BMIJohn = massJohn / (heightJohn * heightJohn)
-
-
-var markHigherBMI = BMIMark > BMIJohn;
-console.log(markHigherBMI);
-console.log( BMIMark, BMIJohn);
-
-
-
 // The switch Statement //
 
 const language = 'arabic';
@@ -142,7 +123,6 @@ switch (language) {
 
 }
 
-*/
 
 // The Conditional(Ternary) Operator //
 
@@ -155,3 +135,76 @@ const result = population > 33? "Portugal's population is above average":
  console.log(`I want to know ${population > 33? "Portugal's population is above average":
  "Portugal's population is below average"}`);
  
+
+ 
+ // coding challenge 1 //
+// Data 1//
+const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark)
+
+
+const BMIJohn = massJohn / (heightJohn * heightJohn)
+
+
+var markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+console.log( BMIMark, BMIJohn);
+
+
+
+
+//  Coding challenge 2 // 
+
+if(BMIMark > BMIJohn){
+    console.log(`Mark's BMI ${BMIMark} is higher than John's BMI ${BMIJohn}!`);
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's BMI ${BMIMark}!`);
+}
+
+
+
+Coding Challenge 3 //
+
+const scoreDolphins = (96 + 108 + 89)/3;
+const scoreKoalas = (88 + 91 + 10)/3;
+
+
+if (scoreDolphins > scoreKoalas){
+    console.log('Dolpins win the trophy🏆');
+} else if (scoreKoalas > scoreDolphins){
+    console.log('Koalas win the trophy🏆');
+} else if (scoreDolphins === scoreKoalas ){
+    console.log("Both win the trophy");
+} else {
+    console.log('no team win the trophy');
+}
+
+// Bonus 1 //
+
+const scoreDolphins = (97 + 112 + 101)/3;
+const scoreKoalas = (109 + 95 + 106)/3;
+
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100){
+    console.log('Dolpins win the trophy🏆');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100){
+    console.log('Koalas win the trophy🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100){
+    console.log("Both win the trophy");
+} else {
+    console.log('no team win the trophy');
+}
+
+*/
+
+// Coding challenges //
+
+const bill = 275;
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
