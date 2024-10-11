2272cf02d5345eec66bbc8d24d513f9f931e7fe8
Youssef Shaker <youssefaymanshaker@outlook.com>
2024-10-11 19:26:27 +0300

    Finished Variables and Values Video

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js = 'amazing';
+// if (js === 'amazing') alert("JAVASCRIPT IS FUN!");
+console.log(48 * 2);
+
+let firstName = "Youssef";
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let _function = 28;
+let _new = "NEW";
+let name = "Youssef Shaker";  // Reserved word legal to use.
+
+const PI = 3.1415;
+
+
+let country = "Egypt";
+let continent = "Middle East";
+let population = 11_000_000;
+
+console.log(country)
+console.log(continent)
+console.log(population)
