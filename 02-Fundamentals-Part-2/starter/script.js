a3f3c4de7b4df4472385e19e365504cfb084897a
Youssef Shaker <youssefaymanshaker@outlook.com>
2024-10-20 08:09:54 +0300

    Finshed Fundamentals Part II

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -50,3 +50,46 @@ console.log(jonas.calcAge4())
 console.log(jonas.age)
 
 console.log(`${jonas.firstName} is a ${jonas.age}-year old teacher and he has ${jonas.hasDriversLicense ? "a" : "no"} driver's license.`)
+
+/* Write your code below. Good luck! 🙂 */
+const calcBMI = (mass, height) => mass / (height * height);
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+};
+mark.bmi = calcBMI(mark.mass, mark.height)
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+};
+john.bmi = calcBMI(john.mass, john.height)
+
+if (mark.bmi > john.bmi) console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`);
+else if (mark.bmi < john.bmi) console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi}`);
+else console.log(`They're equal (${john.bmi}) (${mark.bmi})`);
+
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
+}
+
+const calcAverage = arr => {
+  let sum = 0;
+  arr.forEach(num => sum += num);
+  console.log(arr);
+  return sum / arr.Length;
+}
+calcAverage(totals);
