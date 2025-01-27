44f18c14537f4a532198438fa75b84c2c59987d5
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-01-27 09:38:08 +0800

    20. Type Conversion and Coercion - end

---
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -0,0 +1,15 @@
+// Type Conversion and coercion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Wilfred"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+
+console.log("23" - "10" - 3);
+console.log("23" + "10" + 3);
