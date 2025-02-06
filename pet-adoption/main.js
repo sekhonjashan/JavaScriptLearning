b2a0f0dde0ba6ed79524f162cbb462af7feea765
Mohamed Altahan <maltahanml@gmail.com>
2025-02-06 17:34:49 +0200

    debug 1-3

--- pet-adoption/main.js
+++ pet-adoption/main.js
@@ -9,3 +9,11 @@ async function start() {
 }
 
 start();
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsdata = await petsPromise.json();
+  console.log(petsdata);
+}
+petsArea();
