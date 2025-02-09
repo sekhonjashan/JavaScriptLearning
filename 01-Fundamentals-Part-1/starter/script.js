e0fe45a114ba924f6090b33929fd9474d672763e
tpeper <torben.peper@googlemail.com>
2025-02-09 12:34:44 +0100

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -13,6 +14,7 @@ console.log(firstName);
 let torben_ana = "TA";
 let $function = 27;
 
+// Variable name conventions
 let person = "jonas";
 let PI = 3.1415;
 
@@ -23,3 +25,24 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 12);
+// console.log(typeof "Torben");
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
