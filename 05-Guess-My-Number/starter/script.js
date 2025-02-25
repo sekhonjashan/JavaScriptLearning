5b1e0438ea157e09ada867af2c88cf0bcd88629a
Jeaninuh <jeanine.8@live.nl>
2025-02-25 16:41:19 +0100

    Finished section 7 challenge 1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,69 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    score--;
+    document.querySelector('.score').textContent = score;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  // Compare score to high score
+  if (score > highScore) {
+    document.querySelector('.highscore').textContent = score;
+  }
+
+  // Restore score and regenerate new random number
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Restore guess input, number box and message
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  // Restore bg and number width
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
