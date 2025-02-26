e7692734dc1ca98d3481e8e3bd8c5a37d68e3f30
Jeaninuh <jeanine.8@live.nl>
2025-02-26 08:53:00 +0100

    finished the pig game and section 7

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -116,10 +116,30 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  player0El.classList.add('player--active');
+  player0El.classList.remove('player--active');
+
+  diceEl.classList.add('hidden');
+};
 
 const switchPlayer = function () {
   currentScore = 0;
@@ -128,6 +148,8 @@ const switchPlayer = function () {
   activePlayer = activePlayer === 0 ? 1 : 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
+
+  // diceEl.classList.add('hidden');
 };
 
 // Rolling dice functionality
@@ -161,7 +183,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -178,3 +200,15 @@ btnHold.addEventListener('click', function () {
   } else {
   }
 });
+
+btnNew.addEventListener('click', init);
+
+/*
+When you click "New game":
+- current score to 0
+- Global score to 0
+- Hide dice roll
+- player 0 becomes the first player 
+- remove player--winner from winner
+- give player 0 the player--active styles
+*/
